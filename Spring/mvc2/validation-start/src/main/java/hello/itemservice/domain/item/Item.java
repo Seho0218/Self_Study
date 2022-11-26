0b634385299372fe19cd2d8d9fe89b51ca91a938
Seho0218 <ghdtpgh8913@gmail.com>
2022-11-26 19:31:43 +0900

    Bean Validation

--- Spring/mvc2/validation-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring/mvc2/validation-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,17 +12,17 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10000원 이상")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+    //@NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank(message ="공백 ㄴㄴ", groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotBlank(message ="공백 ㄴㄴ", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min=1000, max=1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer quantity;
 
     public Item() {
