120a97582218be3c82c5f1474889c15212ebb128
Seho0218 <ghdtpgh8913@gmail.com>
2022-12-19 14:18:03 +0900

    회원 도메인 개발 - 회원 서비스 개발

--- Spring/jpa/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ Spring/jpa/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,18 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.PersistenceUnit;
 import jpabook.jpashop.domain.item.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;//생성자로 injection
 
     public void save(Member member) {
         em.persist(member);
