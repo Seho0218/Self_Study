abd9abdc92f059941f58a7b05e413c0c7cae0cc3
Seho0218 <ghdtpgh8913@gmail.com>
2023-01-31 11:48:48 +0900

    Seho0218 (#5)
    * 주문결제 확인, 카테고리 및 태그별 검색 확인
    
    * 로그인 로직 수정 및 비밀번호 변경 확인.
    메서드 수정
    
    * 로그인 수정 및 깃 재활
    
    * 비밀번호 찾기시 변경 가능, 회원정보 변경 가능
    
    * 배송지 복합키 문제 해결

--- project/GenieProject(jpa)/GenieProject/src/test/java/com/genie/myapp/controller/UserControllerTest.java
+++ project/GenieProject(jpa)/GenieProject/src/test/java/com/genie/myapp/controller/UserControllerTest.java
@@ -1,12 +1,19 @@
 package com.genie.myapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.transaction.Transactional;
 
+
+@Slf4j
+@Transactional
 class UserControllerTest {
 
     @Test
-    void userEditOk() {
+    void addDelivery() {
+
+
+
     }
 }
