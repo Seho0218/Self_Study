7e601930a0995d1cd2ad72cc2959a3c1d5f6028b
Seho0218 <ghdtpgh8913@gmail.com>
2023-02-05 01:53:25 +0900

    해쉬 작업

--- project/GenieProject(jpa)/GenieProject/src/main/java/com/genie/myapp/entity/MyOrder.java
+++ project/GenieProject(jpa)/GenieProject/src/main/java/com/genie/myapp/entity/MyOrder.java
@@ -52,4 +52,48 @@ public class MyOrder implements Serializable {
 
     private OrderStatus status;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MyOrder myOrder = (MyOrder) o;
+
+        if (orderCount != myOrder.orderCount) return false;
+        if (productId != myOrder.productId) return false;
+        if (orderPrice != myOrder.orderPrice) return false;
+        if (orderQty != myOrder.orderQty) return false;
+        if (!orderNum.equals(myOrder.orderNum)) return false;
+        if (!genieId.equals(myOrder.genieId)) return false;
+        if (!productName.equals(myOrder.productName)) return false;
+        if (!recipientName.equals(myOrder.recipientName)) return false;
+        if (!recipientPhone.equals(myOrder.recipientPhone)) return false;
+        if (!recipientAddress.equals(myOrder.recipientAddress)) return false;
+        if (!recipientRequest.equals(myOrder.recipientRequest)) return false;
+        if (!recipientDeliveryStatus.equals(myOrder.recipientDeliveryStatus)) return false;
+        if (!paymentMethod.equals(myOrder.paymentMethod)) return false;
+        if (!orderWriteDate.equals(myOrder.orderWriteDate)) return false;
+        return status == myOrder.status;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (orderCount ^ (orderCount >>> 32));
+        result = 31 * result + orderNum.hashCode();
+        result = 31 * result + genieId.hashCode();
+        result = 31 * result + productId;
+        result = 31 * result + productName.hashCode();
+        result = 31 * result + recipientName.hashCode();
+        result = 31 * result + recipientPhone.hashCode();
+        result = 31 * result + recipientAddress.hashCode();
+        result = 31 * result + recipientRequest.hashCode();
+        result = 31 * result + recipientDeliveryStatus.hashCode();
+        result = 31 * result + orderPrice;
+        result = 31 * result + orderQty;
+        result = 31 * result + paymentMethod.hashCode();
+        result = 31 * result + orderWriteDate.hashCode();
+        result = 31 * result + status.hashCode();
+        return result;
+    }
 }
